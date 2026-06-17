1adac6227851fa52bb95d801b1495f52e319f76c
ªMkUsH <138127700+Amkushu999@users.noreply.github.com>
2026-06-17 13:35:31 +0300

    Bump JVM target to 17: jvmToolchain(17), VERSION_17, suppress SDK/MPP warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 android {
     namespace = "com.wangyiheng.vcamsx"
     compileSdk = 34
@@ -33,11 +37,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
